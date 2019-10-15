1bf5a554a4be2d8b5d37787281b4cc7411b6e784
nicolas <nicolas.vercheval@gmail.com>
2019-10-15 20:23:37 +0200

    New list of Features

--- libraries/feature_extraction.py
+++ libraries/feature_extraction.py
@@ -42,21 +42,23 @@ def get_shoulder_angles(pose):
   right_angle = np.math.atan2(np.linalg.det([v0,v1]),np.dot(v0,v1))
   return np.degrees(left_angle), np.degrees(right_angle)
 # To measure if a finger is open or closed
-def arclength(x,size):
+def arclength(x):
   Sum=0
+  x=x[x!=0]
+  size=len(x)
   for i in range(1,size):
-    if x[i].all() and x[i-1].all():
-       Sum+=dist(x[i],x[i-1])
-  return dist(x[size-1],x[0])/(Sum+1e-5)
+    Sum+=dist(x[i],x[i-1])
+  return dist(x[-1],x[0])/(Sum+1e-5)
 
 def finger_openness(sample):
   # @title finger open/closed
   n_fingers = 10
-  finger_openness_feature=np.zeros(len(sample), n_fingers)
+  finger_openness_feature=np.zeros((len(sample), n_fingers))
   for j, frame in enumerate(sample):
     _, _, hand_L, hand_R= get_frame_parts(frame)
     for k in range(int(n_fingers / 2)):
       # Left Hand
-      finger_openness_feature[ j, k] = arclength(hand_L[1 + 4 * k:5 + 4 * k], size=4)
+      finger_openness_feature[ j, k] = arclength(hand_L[1 + 4 * k:5 + 4 * k])
       # Right Hand
-      finger_openness_feature[ j, k + 5] = arclength(hand_R[1 + 4 * k:5 + 4 * k], size=4)
+      finger_openness_feature[ j, k + 5] = arclength(hand_R[1 + 4 * k:5 + 4 * k])
+  return finger_openness_feature
