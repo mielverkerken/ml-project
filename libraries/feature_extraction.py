473a7f1defeaf77965fc7f76f5f06e3363a98c84
nicolas <nicolas.vercheval@gmail.com>
2019-10-15 20:13:56 +0200

    New list of Features

--- libraries/feature_extraction.py
+++ libraries/feature_extraction.py
@@ -1,4 +1,5 @@
 from util.constants import *
+from libraries.base  import *
 import numpy as np
 
 def get_arm_angles(pose):
@@ -40,5 +41,26 @@ def get_shoulder_angles(pose):
 
   right_angle = np.math.atan2(np.linalg.det([v0,v1]),np.dot(v0,v1))
   return np.degrees(left_angle), np.degrees(right_angle)
+# To measure if a finger is open or closed
+def arclength(x,size):
+  Sum=0
+  for i in range(1,size):
+    if x[i].all() and x[i-1].all():
+       Sum+=dist(x[i],x[i-1])
+  return dist(x[size-1],x[0])/(Sum+1e-5)
 
-def ge
+def finger_openness(sample):
+  # @title finger open/closed
+  n_fingers = 10
+  finger_openness_feature=np.zeros(len(sample), n_fingers)
+  for j, frame in enumerate(sample):
+    get_frame_parts(frame)
+    for k in range(int(n_fingers / 2)):
+      # Right Hand
+      finger_openness_feature[ j, k] = arclength(frame[2][1 + 4 * k:5 + 4 * k], size=4)
+      # Right Hand
+      finger_openness_feature[ j, k + 5] = arclength(frame[3][1 + 4 * k:5 + 4 * k], size=4)
+
+  autorijden = finger_openness_feature[(all_labels == 5)]
+  af = finger_openness_feature[(all_labels == 0)]
+  print("Autorijden mean:", nonzeromean(autorijden.flatten()), "Af mean:", nonzeromean(af.flatten()))
