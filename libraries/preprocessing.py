a92a840d6ff35d2a6a96db9e67fd567990913139
soved <devosarthur@gmail.com>
2019-11-13 14:07:08 +0100

    Included preprocessing for included features: - keypoints_nan_to_zero:   Maps nans to zero (quickfix for distance features) - preprocess_test:   Including the keypoints_nan_to_zero within the preprocessing

--- libraries/preprocessing.py
+++ libraries/preprocessing.py
@@ -1,25 +1,42 @@
 import numpy as np
-from util.constants import * 
+from util.constants import *
+
 
 def normilizeSample(sample):
-  # Normilize keypoint x and y coordinate by subtracting mean and dividing by std of sample
-  mean = np.append(np.nanmean(sample[:, :, 0:2], axis=(0,1)), 0)
-  std = np.append(np.nanstd(sample[:, :, 0:2], axis=(0,1)), 1)
-  return (sample - mean) / std
+	# Normilize keypoint x and y coordinate by subtracting mean and dividing by std of sample
+	mean = np.append(np.nanmean(sample[:, :, 0:2], axis=(0, 1)), 0)
+	std = np.append(np.nanstd(sample[:, :, 0:2], axis=(0, 1)), 1)
+	return (sample - mean) / std
+
 
 def remove_missing_keypoints(sample):
-  missing_removed = np.copy(sample)
-  # Replace x and y coordinate with 0 if confidence is negative
-  missing_removed[missing_removed[:,:,2] < 0] = 0
-  # Only replacing x and y coordinate of 0 with nan
-  missing_removed[:, :, 0:2][missing_removed[:, :, 0:2] == 0] = np.nan
-  return missing_removed
+	missing_removed = np.copy(sample)
+	# Replace x and y coordinate with 0 if confidence is negative
+	missing_removed[missing_removed[:, :, 2] < 0] = 0
+	# Only replacing x and y coordinate of 0 with nan
+	missing_removed[:, :, 0:2][missing_removed[:, :, 0:2] == 0] = np.nan
+	return missing_removed
+
+
+def keypoints_nan_to_zero(sample):
+	nan_to_zero = np.copy(sample)
+	# Only replacing x and y coordinate of nan to zero
+	nan_to_zero[nan_to_zero[:, :, 2] == 0] = 0
+	return nan_to_zero
+
 
 def preprocess_sample(sample):
-    pre_sample = remove_missing_keypoints(sample)
-    return normilizeSample(pre_sample)
+	pre_sample = remove_missing_keypoints(sample)
+	return normilizeSample(pre_sample)
+
 
 def preprocess(all_samples, verbose=False):
-  if verbose:
-    print(f"Preprocessing {len(all_samples)} samples")
-  return np.array([preprocess_sample(s) for s in all_samples])
+	if verbose:
+		print(f"Preprocessing {len(all_samples)} samples")
+	return np.array([preprocess_sample(s) for s in all_samples])
+
+
+def preprocess_test(all_samples, verbose=False):
+	if verbose:
+		print(f"Preprocessing {len(all_samples)} samples")
+	return np.array([keypoints_nan_to_zero(preprocess_sample(s)) for s in all_samples])
